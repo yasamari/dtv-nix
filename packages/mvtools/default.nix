0331c743fc1bd8c414dd373cfc3e9824083691f6
yasamari <157554318+yasamari@users.noreply.github.com>
2026-05-19 00:48:46 +0900

    mvtoolsを更新

--- packages/mvtools/default.nix
+++ packages/mvtools/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 pkgs.stdenv.mkDerivation rec {
   pname = "mvtools";
-  version = "2.7.46";
+  version = "2.7.47";
 
   src = pkgs.fetchFromGitHub {
     owner = "pinterf";
     repo = "mvtools";
     tag = version;
-    hash = "sha256-7KQgr/zVsa4IHs9KvwpILA+Lr6O0ybhy79cDV53Um3U=";
+    hash = "sha256-G3/xsfzgtDY3AHiF0N4mfTio9XsD54LYvfi4UTIPngI=";
   };
 
   nativeBuildInputs = [ pkgs.cmake ];
