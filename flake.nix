687db7298534a0d4cad06f4d9a3c4b56a261cb1d
yasamari <157554318+yasamari@users.noreply.github.com>
2026-05-31 16:33:22 +0900

    nixpkgsを更新し、intel-media-sdkのオーバーレイを削除

--- flake.nix
+++ flake.nix
@@ -16,14 +16,6 @@
             "intel-media-sdk-23.2.2"
           ];
         };
-        overlays = [
-          (_self: super: {
-            intel-media-sdk = super.intel-media-sdk.overrideAttrs (old: {
-              cmakeFlags = old.cmakeFlags ++ [ "-DCMAKE_CXX_STANDARD=17" ];
-              NIX_CFLAGS_COMPILE = "-std=c++17";
-            });
-          })
-        ];
       };
     };
 }
