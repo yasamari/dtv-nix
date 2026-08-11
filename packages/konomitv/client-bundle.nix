953ce652f956b7e4a97d601cd7b9f4b9ab837273
yasamari <157554318+yasamari@users.noreply.github.com>
2026-08-12 06:36:00 +0900

    KonomiTVを更新

--- packages/konomitv/client-bundle.nix
+++ packages/konomitv/client-bundle.nix
@@ -29,7 +29,7 @@ pkgs.stdenv.mkDerivation rec {
 
   yarnOfflineCache = pkgs.fetchYarnDeps {
     yarnLock = konomitvSrc + "/client/yarn.lock";
-    hash = "sha256-yuYXbVOaJPOpBsfteSC6mKLqCOZOUD4TTSnd9mYan1U=";
+    hash = "sha256-rgTpwY7psIE8WUhN45SD8dyAzT+YErJWB/7nDY4wZW0=";
   };
 
   installPhase = ''
--- packages/konomitv/default.nix
+++ packages/konomitv/default.nix
@@ -5,15 +5,15 @@
   ...
 }:
 let
-  version = "0.14.1-unstable-2026-08-03";
+  version = "0.14.1-unstable-2026-08-11";
   python = pkgs.python313;
   py = python.pkgs;
 
   konomitvSrc = pkgs.fetchFromGitHub {
     owner = "tsukumijima";
     repo = "KonomiTV";
-    rev = "feccfab827ed9dff73f693efe88ba271129fa514";
-    hash = "sha256-dorPRsr4C3nbxDWAHGBJRGG+v87w7lQg8xP4zrsbAms=";
+    rev = "d84836d87dc2e4c5954a1fd055698dd891df1cd8";
+    hash = "sha256-aXG4wOCql8OcHFHAp0L1k2QMk8CtVzSytFXb5JBBQHQ=";
   };
 
   clientBundle = import ./client-bundle.nix {
